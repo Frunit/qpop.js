15b25759e11eae9fb6150d507116ab1ddede9c18
Frunit <qpop@frunit.de>
2019-11-19 20:34:08 +0200

    Added outro animations

--- outro.js
+++ outro.js
@@ -1,18 +1,19 @@
 'use strict';
 
-// TODO: Outro!
+
+// MAYBE: At the moment, the outro is not cancelable. Should it be? What should happen?
+
 
 function Outro(winner) {
 	this.id = SCENE.OUTRO;
 	this.winner = winner; // negative -> game is lost
-	this.pic = resources.get('gfx/dummy_outro.png');
 
 	// CONST_START
-	this.pic_dim = [595, 415];
-	this.pic_offset = [23, 42];
+	this.anim_dim = [600, 420];
+	this.anim_offset = [19, 39];
 	// CONST_END
 
-	this.frame = 0;
+	this.animation = null;
 
 	this.clickareas = [];
 }
@@ -20,6 +21,8 @@ function Outro(winner) {
 
 Outro.prototype.initialize = function() {
 	canvas.style.cursor = 'default';
+	const num = (this.winner >= 0) ? this.winner : 6;
+	this.animation = new Animation(outro_frames[num], this.anim_offset);
 	this.redraw();
 };
 
@@ -30,22 +33,19 @@ Outro.prototype.redraw = function() {
 	draw_rect([0, 20], [640, 460]); // Main rectangle
 
 	// Inverted rectangle around the picture
-	draw_inv_rect([this.pic_offset[0] - 1, this.pic_offset[1] - 1], [this.pic_dim[0] + 2, this.pic_dim[1] + 2]);
-
-	this.clickareas = [];
-
-	// Background panels
-	ctx.drawImage(this.pic, this.pic_offset[0], this.pic_offset[1]);
+	draw_inv_rect([this.anim_offset[0] - 1, this.anim_offset[1] - 1], [this.anim_dim[0] + 2, this.anim_dim[1] + 2]);
 };
 
 
 Outro.prototype.render = function() {
-
+	this.animation.render();
 };
 
 
 Outro.prototype.update = function() {
-
+	if(!this.animation.has_stopped) {
+		this.animation.step();
+	}
 };
 
 
