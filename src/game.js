c4a8fffa1b8682d66fce669de7b6d218e2941b41
Frunit <qpop@frunit.de>
2020-08-29 15:14:31 +0200

    Version push to 1.0.0 :)

--- src/game.js
+++ src/game.js
@@ -1007,7 +1007,7 @@ document.addEventListener('visibilitychange', handle_visibility_change);
 
 let lang = null;
 
-const version = [0, 9, 10];
+const version = [1, 0, 0];
 const game = new Game();
 game.initialize();
 game.start();
